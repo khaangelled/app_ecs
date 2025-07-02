d591b6f08896de4021f38fc953d9d330e1ab4e07
khaangelled <khangelled@gmail.com>
2025-07-02 11:44:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ with col2:
             <div style="text-align:center;">
                 <img
                     src="data:image/png;base64,{img_b64}"
-                    style="width:350; height:auto; cursor:pointer;"
+                    style="width:350; height:350; cursor:pointer;"
                     onclick="window.open(this.src)"
                     alt="Preview Image"
                 />
